ce1ee6797bedf69a33a530f6249f54534fa889c2
unknown <Henning@.(none)>
2011-08-29 12:48:40 +0200

    Player with "content"

--- src/de/pokerdude/Player.java
+++ src/de/pokerdude/Player.java
@@ -1,5 +1,20 @@
 package de.pokerdude;
 
-public class Player {
+import java.util.ArrayList;
 
+
+
+public class Player {
+	ArrayList<Card> Cards;
+	
+	public Player() {
+	}
+	
+	public ArrayList<Card> getCards() {
+		return Cards;
+	}
+	
+	public void setCards(ArrayList<Card> Cards) {
+		this.Cards = Cards;
+	}
 }
