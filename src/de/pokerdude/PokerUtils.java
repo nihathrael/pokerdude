8807f2fb5d8feba5f75326f45a75e35b5e53d218
Thomas Kinnen <kinnen@aviamo.de>
2011-09-14 12:32:47 +0200

    New pokerutils function

--- src/de/pokerdude/PokerUtils.java
+++ src/de/pokerdude/PokerUtils.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class PokerUtils {
 	
-	public static int evaluateCards(ArrayList<Card> hand, ArrayList<Card> flop, Card turn, Card river) {
+	public static Powerrating evaluateCards(ArrayList<Card> hand, ArrayList<Card> flop, Card turn, Card river) {
 		ArrayList<Card> allCards = new ArrayList<Card>(7);
 		allCards.addAll(hand);
 		allCards.addAll(flop);
 		allCards.add(turn);
 		allCards.add(river);
 		CardSet cardset = new CardSet(allCards);
-		return 0;
+		return cardset.evaluate();
 	}
 	
 	public static int calcFlush() {
