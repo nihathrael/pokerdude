7fb5651dcb7e2b7c3717acaf022ef2b63719143d
Thomas Kinnen <thomas.kinnen@gmail.com>
2011-09-26 17:55:11 +0200

    Added documentation * New Playertype * Added 10000 turn preflop

--- src/de/pokerdude/players/Player.java
+++ src/de/pokerdude/players/Player.java
@@ -3,12 +3,15 @@ package de.pokerdude.players;
 import java.util.ArrayList;
 
 import de.pokerdude.actions.CallAction;
+import de.pokerdude.actions.FoldAction;
 import de.pokerdude.actions.PokerAction;
 import de.pokerdude.actions.RaiseAction;
 import de.pokerdude.game.Card;
+import de.pokerdude.game.CardSet;
 import de.pokerdude.game.GameState;
 import de.pokerdude.game.PokerGame;
 import de.pokerdude.opponentmodeling.OpponentModelTable;
+import de.pokerdude.utils.Powerrating;
 
 
 
@@ -28,7 +31,7 @@ public class Player {
 	}
 	 
 	public ArrayList<Card> getCards() {
-		return Cards;
+		return new ArrayList<Card>(Cards);
 	}
 	
 	public void setCards(ArrayList<Card> Cards) {
@@ -50,19 +53,29 @@ public class Player {
 		}
 	}
 	
-	public PokerAction getBetPreFlop(int i, boolean b) {
+	public PokerAction getBetPreFlop(int minBet, boolean b) {
 		if(b) {
-			return new RaiseAction(game, this, i);
+			return new RaiseAction(game, this, minBet);
+		}
+		if(Math.random() > 0.5) {
+			return new RaiseAction(game, this, minBet+10);
 		}
 		return new CallAction(game, this);
 	}
 	
 	public PokerAction getBetPreTurn(int minBet) {
+		ArrayList<Card> allCards = getCards();
+		allCards.addAll(game.getCommonCards());
+		CardSet cardset = new CardSet(allCards);
+		Powerrating rating = cardset.evaluate();
+		if(rating.compareTo(new Powerrating(new int[] {2, 0})) <= 0) {
+			new FoldAction(game, this);
+		}
 		return new CallAction(game, this);
 	}
 	
 	public PokerAction getBetPreRiver(int minBet) {
-		return new CallAction(game, this);
+		return getBetPreTurn(minBet);
 	}
 	
 	public String toString() {
